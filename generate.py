3ad8741221a1b4709ee8840e662ec91d665b710c
Tobias Kunze <r@rixx.de>
2018-07-13 23:47:28 +0200

    Code style fixes

--- generate.py
+++ generate.py
@@ -5,10 +5,12 @@ import pandas as pd
 
 
 # Configuration, or what passes as such
-suptitle_font = {'family':'Titillium', 'fontsize':60, 'fontweight':'normal', 'y':1.07}
-title_font = {'family':'Titillium', 'size':24, 'weight':'bold'}
-xtick_font = {'family':'Titillium', 'size':10, 'weight':'bold', 'alpha':1.0, 'zorder':3}
-ytick_font = {'family':'Titillium', 'size': 9, 'weight':'bold', 'alpha':0.2, 'zorder':3}
+font = {'family': 'Titillium'}
+tick_font = {'weight': 'bold', 'zorder': 3}
+supertitle_font = {'fontsize': 60, 'y': 1.07, **font}
+title_font = {'size': 24, 'weight': 'bold', **font}
+xtick_font = {'size': 10, 'alpha': 1.0, **font, **tick_font}
+ytick_font = {'size': 9, 'alpha': 0.2, **font, **tick_font}
 
 ox.config(log_console=True, use_cache=True)
 weight_by_length = False
@@ -38,24 +40,24 @@ def polar_plot(ax, bearings, slices=36, title=''):
     _, division = np.histogram(bearings, bins=bins)
     frequency = count / count.sum()
     division = division[0:-1]
-    width =  2 * np.pi / slices
+    width = 2 * np.pi / slices
 
     ax.set_theta_zero_location('N')
     ax.set_theta_direction('clockwise')
 
     x = division * np.pi / 180
-    bars = ax.bar(
+    ax.bar(
         x, height=frequency, width=width, align='center', bottom=0, zorder=2,
         color='#003366', edgecolor='k', linewidth=0.5, alpha=0.7,
     )
     ax.set_ylim(top=frequency.max())
     ax.set_title(title.upper(), y=1.05, fontdict=title_font)
-    
+
     ax.set_yticks(np.linspace(0, max(ax.get_ylim()), 5))
     yticklabels = ['{:.2f}'.format(y) for y in ax.get_yticks()]
     yticklabels[0] = ''
     ax.set_yticklabels(labels=yticklabels, fontdict=ytick_font)
-    
+
     xticklabels = ['N', '', 'E', '', 'S', '', 'W', '']
     ax.set_xticklabels(labels=xticklabels, fontdict=xtick_font)
     ax.tick_params(axis='x', which='major', pad=-2)
@@ -67,11 +69,10 @@ def count_and_merge(n, bearings):
     n = n * 2
     bins = [ang * 360 / n for ang in range(0, n + 1)]
     count, _ = np.histogram(bearings, bins=bins)
-    
-    # move the last bin to the front, so eg 0.01° and 359.99° will be binned together
+
+    # Move the last bin to the front to merge e.g. 0.01° and 359.99°
     count = count.tolist()
     count = [count[-1]] + count[:-1]
-
     count_merged = []
     count_iter = iter(count)
     for count in count_iter:
@@ -87,33 +88,49 @@ def get_bearing(place):
         graph = ox.graph_from_place(query, network_type='drive')
     except Exception:
         return
-    
+
     # calculate edge bearings
     graph = ox.add_edge_bearings(graph)
-    
+
     if weight_by_length:
         # weight bearings by length (meters)
-        streets = [(d['bearing'], int(d['length'])) for u, v, k, d in graph.edges(keys=True, data=True)]
+        streets = [
+            (d['bearing'], int(d['length']))
+            for u, v, k, d in graph.edges(keys=True, data=True)
+        ]
         city_bearings = []
         for street in streets:
             city_bearings.extend([street[0]] * street[1])
         return pd.Series(city_bearings)
     else:
         # don't weight bearings, just take one value per street segment
-        return pd.Series([data['bearing'] for u, v, k, data in graph.edges(keys=True, data=True)])
+        return pd.Series([
+            data['bearing']
+            for u, v, k, data in graph.edges(keys=True, data=True)
+        ])
 
 
 if __name__ == '__main__':
     gdf = ox.gdf_from_places(places.values())
-    bearings = {place: get_bearing(place) for place in sorted(places.keys())}
-    bearings = {key: value for key, value in bearings.items() if value is not None}
+    bearings = {
+        place: get_bearing(place)
+        for place in sorted(places.keys())
+    }
+    bearings = {
+        key: value
+        for key, value in bearings.items()
+        if value is not None
+    }
 
     # create figure and axes
     n = len(places)
     ncols = int(np.ceil(np.sqrt(n)))
     nrows = int(np.ceil(n / ncols))
     figsize = (ncols * 5, nrows * 5)
-    fig, axes = plt.subplots(nrows, ncols, figsize=figsize, subplot_kw={'projection':'polar'})
+    fig, axes = plt.subplots(
+        nrows, ncols, figsize=figsize,
+        subplot_kw={'projection': 'polar'},
+    )
     axes = [item for sublist in axes for item in sublist]
 
     # plot each city's polar histogram
@@ -122,7 +139,7 @@ if __name__ == '__main__':
             polar_plot(ax, bearings[place], title=place)
 
     # Add super title and save full image
-    fig.suptitle('City Street Network Orientation', **suptitle_font)
+    fig.suptitle('City Street Network Orientation', **supertitle_font)
     fig.tight_layout()
     fig.subplots_adjust(hspace=0.35)
     plt.gcf().savefig('images/street-orientations.png', dpi=120, bbox_inches='tight')
