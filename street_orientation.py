25ad1fd55005597c53163544fc7b7689e7669716
Tobias Kunze <r@rixx.de>
2018-07-14 14:59:58 +0200

    Move checking logic into main script

--- street_orientation.py
+++ street_orientation.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import osmnx as ox
 import pandas as pd
+import requests
 
 
 # Configuration, or what passes as such
@@ -163,6 +164,40 @@ def print_list():
     plt.close()
 
 
+def check_places():
+    places = load_places()
+    url = 'https://nominatim.openstreetmap.org/search'
+
+    for place, query in places.items():
+        params = {
+            'format': 'json',
+            'limit': 1,
+            'dedupe': 0,
+            'polygon_geojson': 1
+        }
+        if isinstance(query, str):
+            params['q'] = query
+        else:
+            params = {**params, **query}
+
+        response = requests.get(url, params)
+        if response.status_code != 200:
+            print(f'Got non-200 response for {place}: {response.content.decode()}')
+            continue
+
+        data = json.loads(response.content.decode())
+        if len(data) != 1:
+            print(f'Got non-1 length response for {place}: {data}')
+            continue
+        if 'geojson' not in data[0]:
+            print('Got malformed response without geojson key for {place}: {data}')
+            continue
+        geojson_type = data[0]['geojson']['type']
+        if geojson_type not in ['Polygon', 'MultiPolygon']:
+            print(f'Response for {place} is of type {geojson_type}!')
+            continue
+
+
 def print_help():
     print('Usage: street_orientation.py MODE FILE')
     print('Generates radial histogram plots from the direction of a region\'s streets.')
@@ -177,11 +212,14 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print_help()
         sys.exit(-1)
-    elif sys.argv[1] == 'list':
+    mode = sys.argv[1]
+    if mode == 'list':
         try:
             print_list()
         except Exception as e:
             print(f'Error during list generation: {e}.')
+    elif mode == 'check':
+        check_places()
     else:
         print_help()
         sys.exit(-1)
